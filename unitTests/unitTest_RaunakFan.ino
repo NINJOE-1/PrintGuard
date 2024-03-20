303970c539ef6ce5ffa9ffc48c0c4fbc62d4c7cd
RaunakSinghSoi <144322027+RaunakSinghSoi@users.noreply.github.com>
2024-03-20 12:00:56 -0700

    Update unitTest_RaunakFan.ino

--- unitTests/unitTest_RaunakFan.ino
+++ unitTests/unitTest_RaunakFan.ino
@@ -1,4 +1,4 @@
-//written by Raunak with help from Joseph , edited to accomadate the altitude factor to make testing realistic.
+//written by Raunak
 #include <Arduino.h>
 
 uint8_t fan = 9;    // Fan pin
