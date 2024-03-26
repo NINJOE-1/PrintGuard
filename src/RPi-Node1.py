84351897306d24e514197ee94e97d4db51de935d
Joseph Vretenar <josephvretenar@cmail.carleton.ca>
2024-03-26 14:19:03 -0400

    added firebase functionality

--- src/RPi-Node1.py
+++ src/RPi-Node1.py
@@ -1,10 +1,14 @@
 import smbus
 import sys
-#import pyrebase
+import pyrebase
+import socket
+import discord
+from discord.ext import commands
 from time import sleep
 import info
 import PrusaLinkWrapper
-'''
+from datetime import datetime
+
 config = {
     "apiKey": info.firebaseAPI,
     "authDomain": info.firebaseAuth,
@@ -16,34 +20,51 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 printer = PrusaLinkWrapper.PrusaLinkWrapper(info.printerIP, info.printerKey)
-'''
+
+intents = discord.Intents.default()
+intents.presences = False
+bot_token = info.botToken
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 bus = smbus.SMBus(1)
 fanAddress = 0x08
 sensorAddress = 0x09
 cameraAddress = 0x0a
 
+table1 = "SensorData"
+table2 = "PrinterData"
+
+sensorTable = ["internalTemp", "internalHumidity", "externalTemp", "externalHumidity", "fanSpeed"]
+printerTable = ["nozzleTemp", "bedTemp", "progress"]
+
+async def send_discord_message(bot, message):
+        try:
+            channel = bot.get_channel(info.channelId)
+            await channel.send(message)
+        except Exception as e:
+            print(f"Error sending Discord message: {e}")
+
 def main():
     while True:
-        sleep(1)
         bus.write_byte(sensorAddress, 0x01)
         dhtData = bus.read_i2c_block_data(sensorAddress, 0x00, 4)
-        iTemp = dhtData[0]
-        iHum = dhtData[1]
-        oTemp = dhtData[2]
-        oHum = dhtData[3]
-        print("Inner Temp: " + str(iTemp))
-        print("Inner Hum: " + str(iHum))
-        print("Outer Temp: " + str(oTemp))
-        print("Outer Hum: " + str(oHum))
-        sleep(1)
         bus.write_byte(fanAddress, 0x01)
-        sleep(1)
+        sleep(1)	#wait for rpm to be calculated
         rpmData = bus.read_i2c_block_data(fanAddress, 0x00, 2)
-        print("High byte: " + str(rpmData[0]))
-        print("Low byte: " + str(rpmData[1]))
         rpm = ((rpmData[0] << 8) | rpmData[1])
-        print("Rpm speed is: " + str(rpm))
-        sleep(2)
+        sensorData = [dhtData[0], dhtData[1], dhtData[2], dhtData[3], rpm]
+        
+        nozzle = printer.getNozzle()
+        bed = printer.getBed()
+        progress = printer.getProgress()
+        printerData = [nozzle, bed, progress]
+        
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        for i in sensorTable:
+            db.child(table1).child(sensorTable[i]).child(time).set(sensorData[i])
+        for j in printerTable:
+            db.child(table2).child(printerTable[j]).child(time).set(printerData[j])
+        sleep(5)
 
 if __name__ == '__main__':
     try:
