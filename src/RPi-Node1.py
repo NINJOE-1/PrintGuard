5c92fd32eb51460e7fb750b17f91e5912e526d1d
Joseph <joseph@vretenar.ca>
2024-03-27 13:42:09 -0400

    Update RPi-Node1.py

--- src/RPi-Node1.py
+++ src/RPi-Node1.py
@@ -9,10 +9,7 @@ Written by Joseph Vretenar
 import smbus
 import sys
 import pyrebase
-import socket
-import discord
-from discord.ext import commands
-import asyncio
+import requests
 from time import sleep
 import info
 import PrusaLinkWrapper
@@ -31,12 +28,6 @@ db = firebase.database()
 # setup printer config
 printer = PrusaLinkWrapper.PrusaLinkWrapper(info.printerIP, info.printerKey)
 
-# setup discord config
-intents = discord.Intents.default()
-intents.presences = False
-bot_token = info.botToken
-bot = commands.Bot(command_prefix='!', intents=intents)
-
 # setup i2c busses
 bus = smbus.SMBus(1)
 fanAddress = 0x08
@@ -44,58 +35,74 @@ sensorAddress = 0x09
 cameraAddress = 0x0a
 
 # tables on firebase
-table1 = "SensorData"
-table2 = "PrinterData"
+t1 = "SensorData"
+t2 = "PrinterData"
+t3 = "Errors"
 
-# send discord message function
-async def send_discord_message(bot, message) :
-    try:
-        await bot.wait_until_ready()
-        channel = bot.get_channel(1213212262316376085)
-        await channel.send(message)
-    except Exception as e:
-        print(f"Error sending Discord message: {e}")
 
 # main function
-def main() :
+def main():
     while True:
-        bus.write_byte(sensorAddress, 0x01)		# request data from sensor arduino
-        dhtData = bus.read_i2c_block_data(sensorAddress, 0x00, 4)	# read data from sensor arduino
-        bus.write_byte(fanAddress, dhtData[0])	# write the internal temp to the fan arduino
-        sleep(1)								# wait for rpm to be calculated
-        rpmData = bus.read_i2c_block_data(fanAddress, 0x00, 2)		# read rpm from fan arduino
-        rpm = ((rpmData[0] << 8) | rpmData[1])	# calculate true rpm from data
+        # request data from arduino sensors
+        bus.write_byte(sensorAddress, 0x01)
+        # read data from arduino sensors
+        dhtData = bus.read_i2c_block_data(sensorAddress, 0x00, 4)
+        # write the internal temp to the fan arduino
+        bus.write_byte(fanAddress, dhtData[0])
+        # wait for the rpm to be calculated
+        sleep(1)
+        # read the rpm from the fan arduino
+        rpmData = bus.read_i2c_block_data(fanAddress, 0x00, 2)
+        # calculate true rpm from two bytes
+        rpm = ((rpmData[0] << 8) | rpmData[1])
+        # combine all sensor data
         sensorData = [dhtData[0], dhtData[1], dhtData[2], dhtData[3], rpm]
-        
-        nozzle = printer.getNozzle()			# get nozzle temp
-        bed = printer.getBed()					# get bed temp
+        # get printer sensors
+        nozzle = printer.getNozzle()    # get nozzle temp
+        bed = printer.getBed()          # get bed temp
         printerData = [nozzle, bed]
-        
-        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')			# measure current time
-        
+        # measure current time
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         # record all data on firebase
-        db.child(table1).child("internalTemp").child(time).set(sensorData[0])
-        db.child(table1).child("internalHumidity").child(time).set(sensorData[1])
-        db.child(table1).child("externalTemp").child(time).set(sensorData[2])
-        db.child(table1).child("externalHumidity").child(time).set(sensorData[3])
-        db.child(table1).child("fanSpeed").child(time).set(sensorData[4])
-        db.child(table2).child("nozzleTemp").child(time).set(printerData[0])
-        db.child(table2).child("bedTemp").child(time).set(printerData[1])
-        
+        db.child(t1).child("internalTemp").child(time).set(sensorData[0])
+        db.child(t1).child("internalHumidity").child(time).set(sensorData[1])
+        db.child(t1).child("externalTemp").child(time).set(sensorData[2])
+        db.child(t1).child("externalHumidity").child(time).set(sensorData[3])
+        db.child(t1).child("fanSpeed").child(time).set(sensorData[4])
+        db.child(t2).child("nozzleTemp").child(time).set(printerData[0])
+        db.child(t2).child("bedTemp").child(time).set(printerData[1])
         # if the printer is printing, get progress
         if (printer.getPrintingStatus()):
             progress = printer.getProgress()
-            db.child(table3).child("progress").child(time).set(progress)
-        
-        # check if temperature should provide a warning
+            db.child(t2).child("progress").child(time).set(progress)
+        # check for errors and notify user
         if (sensorData[0] > 35):
-            message = ("Potential error: internal temp is " + str(sensorData[0]))
-            #send_discord_message(bot, message)
-        
+            error = 'Internal temperature is at  ' + str(sensorData[0]) + '°C'
+            payload = {'content': (error)}
+            requests.post(info.DISCORD_WEBHOOK_URL, json=payload)
+            db.child(t3).child("error").child(time).set(error)
+            if (sensorData[4] == 0):
+                error = 'Fan is not running'
+                payload = {'content': (error)}
+                requests.post(info.DISCORD_WEBHOOK_URL, json=payload)
+                db.child(t3).child("error").child(time).set(error)
+        elif (sensorData[0] > 31 and sensorData[4] == 0):
+            error = 'Fan is not running'
+            payload = {'content': (error)}
+            requests.post(info.DISCORD_WEBHOOK_URL, json=payload)
+            db.child(t3).child("error").child(time).set(error)
+        elif (sensorData[0] < 31 and sensorData[4] > 100):
+            error = 'Fan is stuck running'
+            payload = {'content': (error)}
+            requests.post(info.DISCORD_WEBHOOK_URL, json=payload)
+            db.child(t3).child("error").child(time).set(error)
         sleep(5)
+
 
 if __name__ == '__main__':
     try:
+        payload = {'content': 'Print Guard is now operational'}
+        response = requests.post(info.DISCORD_WEBHOOK_URL, json=payload)
         main()
     except KeyboardInterrupt:
         sys.exit(0)
