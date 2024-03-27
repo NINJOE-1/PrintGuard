5a084ae6ea232de6aac83406efa425c4b593e909
Joseph <joseph@vretenar.ca>
2024-03-27 09:08:26 -0400

    Update fanDeployment.ino

--- src/fanDeployment.ino
+++ src/fanDeployment.ino
@@ -4,12 +4,13 @@
 #define PWMPIN 9
 #define RPMPIN 2
 
-uint8_t pwm = 0;
+volatile uint8_t pwm = 0;
 uint32_t count = 0;
 uint8_t data [] = {0, 0};
 unsigned long start_time;
 uint16_t rpm;
 uint8_t incoming;
+uint8_t measure = 0;
 
 void setup() {
   Wire.begin(ADDRESS);
@@ -17,29 +18,33 @@ void setup() {
   Wire.onRequest(sendData);
   pinMode(PWMPIN, OUTPUT);
   pinMode(RPMPIN, INPUT_PULLUP);
-  analogWrite(PWMPIN, pwm);
   attachInterrupt(digitalPinToInterrupt(RPMPIN), counter, RISING);
 }
 
 void loop() {
-  analogWrite(PWMPIN, pwm);
+  if (measure == 1) {
+    count = 0;
+    start_time = millis();
+    while((millis() - start_time) < 1000) {}
+    rpm = (count * 30);
+    measure = 0;
+  }
 }
 
 void receiveData(int byteCount) {
-  count = 0;
-  start_time = millis();
-  while((millis() - start_time) < 1000) {}
-  rpm = (count * 30);
-  while (Wire.available()) {
-    incoming = Wire.read();
-    if (incoming < 31) {
-      pwm = 0;
-    } else if (data >= 31 && data < 34) {
-      pwm = 127;
-    } else {
-      pwm = 255;
-    }
+  incoming = Wire.read();
+  if (incoming == 0) {
+  } else if (incoming < 31) {
+    pwm = 0;
+    analogWrite(PWMPIN, pwm);
+  } else if (incoming >= 31 && incoming < 34) {
+    pwm = 127;
+    analogWrite(PWMPIN, pwm);
+  } else {
+    pwm = 255;
+    analogWrite(PWMPIN, pwm);
   }
+  measure = 1;
 }
 
 void sendData() {
